b7c74b1f67386e99536488b3519d77ee60e3e355
stacyburris <stacy1burris@gmail.com>
2021-07-16 11:53:11 -0700

    testing staircase challenge

--- _test_/index.hr.test.js
+++ _test_/index.hr.test.js
@@ -1,9 +1,19 @@
-// const staircase = require('../code/hackerrank/staircase.js');
+const staircase = require('../code/hackerrank/staircase.js');
 
-// // hacker rank staircase challenge
+// hacker rank staircase challenge
 
-// describe('Solved challenges from hackerrank: ', () => {
-//     let n = 4;
-//     console.log(" ".repeat(n-i) + "#".repeat(i))  
+describe('Staircase challenge ', () => {
+    it('Should print a staircase of size n using # symbols and spaces', () => {
+        let n = 6;
+     for(let i = 1; i <= n; i++){
+     console.log(''.repeat(n-i) + "#".repeat(i))  
+    expect(staircase(n).toBe(6))        
+     }
 
-// })
+    })
+})
+
+// function staircase(n) {
+//     for(let i = 1; i <= n; i++){
+//           console.log(" ".repeat(n-i) + "#".repeat(i))  
+//     }
