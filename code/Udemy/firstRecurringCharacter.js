033230e9d2754af5473702036b5cc7ff37717f0c
stacyburris <stacy1burris@gmail.com>
2021-08-11 16:26:39 -0700

    function two for loops then hash table for efficiency

---
+++ code/Udemy/firstRecurringCharacter.js
@@ -0,0 +1,48 @@
+'use strict';
+
+//Given an array = [2,5,1,2,3,5,1,2,4];
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4];
+//It should return 1
+
+//Given an array = [2,3,4,5];
+//It should return undefined
+
+
+//nested for loop
+//not efficient
+
+function firstRecurringCharacter(input){
+    for(let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) { //1 to the right 
+            if (input[i] === input[j]){
+                return input[i];
+            }
+        }
+    }
+    return undefined;
+} // O(n^2)
+
+
+//hash table!
+// loop through each and add to hash table
+function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+        if(map[input[i]] !== undefined) {
+            return input[i];
+        } else {
+            map[input[i]] = i
+        }
+        console.log(map); 
+    }
+  
+    return undefined;
+    //O(n)
+}
+
+
+firstRecurringCharacter2([2,5,1,2,3,5,1,2,4]);
+
+
